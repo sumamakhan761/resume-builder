fffe9047981877c2d34bf82aa52e9fe8c8519c51
sumama <sumamakhan761>
2024-12-10 16:29:47 +0530

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s - AI Resume Builder",
-    absolute: "AI Resume Builder",
+    template: "%s - Resume Builder",
+    absolute: "Resume Builder",
   },
   description:
     "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Link from "next/link";
 import Card from "./FeatureCards";
 import { Cover } from "@/components/ui/cover";
+import Image from "next/image";
+import icon from "@/assets/form.png";
+import color from "@/assets/color.png";
+import dnd from "@/assets/dnd.png";
+
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
@@ -41,7 +46,7 @@ export default function Home() {
   return (
     <main
       ref={containerRef}
-      className="relative min-h-screen w-full bg-gradient-to-br from-black via-gray-900 to-gray-800"
+      className="relative min-h-screen w-full overflow-x-hidden bg-gradient-to-br from-black via-gray-900 to-gray-800"
     >
       {/* Tracking Beam */}
       <div className="fixed left-8 top-0 h-full w-1">
@@ -77,6 +82,83 @@ export default function Home() {
           </Link>
         </motion.div>
       </section>
+
+      <section className="scroll-section flex flex-col items-center px-4 py-20 md:flex-row-reverse md:px-20">
+        <motion.div
+          className="mb-10 md:mb-0 md:w-1/2"
+          whileInView={{ x: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.8 }}
+        >
+          <Image
+            src={icon}
+            alt="Export Options"
+            width={550}
+            height={400}
+            className="rounded-lg shadow-xl"
+          />
+        </motion.div>
+        <div className="md:w-1/2 md:pr-20">
+          <h2 className="mb-6 text-4xl font-bold text-white">
+            Easy Customization
+          </h2>
+          <p className="text-lg text-gray-300">
+            Customize every aspect of your resume with our intuitive editor
+          </p>
+        </div>
+      </section>
+
+      <section className="scroll-section flex flex-col items-center px-4 py-20 md:flex-row md:px-20">
+        <motion.div
+          className="mb-10 md:mb-0 md:w-1/2"
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.8 }}
+        >
+          <Image
+            src={dnd}
+            alt="Export Options"
+            width={600}
+            height={400}
+            className="rounded-lg shadow-xl"
+          />
+        </motion.div>
+        <div className="md:w-1/2 md:pr-20">
+          <h2 className="mb-6 text-4xl font-bold text-white">
+            Design Your Resume with Ease
+          </h2>
+          <p className="text-lg text-gray-300">
+            Effortlessly design your resume with drag-and-drop functionality.
+            Adjust sections, add a profile picture, and reshape your resume to
+            perfectly fit your style and needs.
+          </p>
+        </div>
+      </section>
+
+      <section className="scroll-section flex flex-col items-center px-4 py-20 md:flex-row-reverse md:px-20">
+        <motion.div
+          className="mb-10 md:mb-0 md:w-1/2"
+          whileInView={{ x: [100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.8 }}
+        >
+          <Image
+            src={color}
+            alt="Export Options"
+            width={600}
+            height={400}
+            className="rounded-lg shadow-xl"
+          />
+        </motion.div>
+        <div className="md:w-1/2 md:pr-20">
+          <h2 className="mb-6 text-4xl font-bold text-white">
+            Personalize Your Resume
+          </h2>
+          <p className="text-lg text-gray-300">
+            Choose from a variety of color palettes and styles to make your
+            resume truly yours. Customize every detail and create a professional
+            look that stands out.
+          </p>
+        </div>
+      </section>
+
       <Card />
     </main>
   );
