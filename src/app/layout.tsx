4c6742f6ef3dcd35a38747d70229d5e9d7d92fae
sumamakhan7611 <khansumama87111@gmail.com>
2025-03-27 09:53:04 +0530

    docker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
